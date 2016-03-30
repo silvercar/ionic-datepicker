a630b9b6b72e89aaa19f331afd67666906e021bd
Dan Emos <george.nickas@silvercar.com>
2016-03-30 11:51:37 -0500

    Test commit.

--- src/ionic-datepicker.provider.js
+++ src/ionic-datepicker.provider.js
@@ -2,7 +2,7 @@ angular.module('ionic-datepicker.provider', [])
 
   .provider('ionicDatePicker', function () {
 
-        alert('AAAAAAAAAAA!!!!');
+        alert('BBBBB!!!!');
     var config = {
       setLabel: 'Set',
       todayLabel: 'Today',
